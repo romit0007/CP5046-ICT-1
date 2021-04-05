957dc44a5e0fd7fd5c3ddf5810385290515f05fc
romitb1996 <76085614+romitb1996@users.noreply.github.com>
2021-04-05 16:13:50 +1000

    booking Page
    Added Booking page components

--- src/components/BookA/BookAElements.js
+++ src/components/BookA/BookAElements.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const BookAElements = () => {
-    return (
-        <BookAContainer>
-            <BookAWrap>
-                
-            </BookAWrap>
-        </BookAContainer>
-    )
-}
-
-export default BookAElements
+export const Container = styled.div`
+    min-height: 692px;
+    position: fixed;
+    bottom:0;
+    left:0;
+    right:0;
+    top:0;
+    
+`
--- src/components/BookA/index.js
+++ src/components/BookA/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { FormLabel } from 'react-bootstrap'
+import FormCheckInput from 'react-bootstrap/esm/FormCheckInput'
 
-const index = () => {
+const BookA = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <FormWrap>
+                <Icon to ='/'>BLM</Icon>
+                <FormContent>
+                    <Form action = '/'>
+                        <FormH1>Book an appointment here</FormH1>
+                        <FormLabel htmlFor='for'> First Name </FormLabel>
+                        <FormCheckInput type = 'text' required/>
+                        <FormLabel htmlFor='for'>Last Name</FormLabel>
+                        <FormCheckInput type = 'text' required/>
+                        <FormLabel htmlFor='for'>Mobile Number</FormLabel>
+                        <FormCheckInput type = 'number' required/>
+                        <FormLabel htmlFor='for'>Email</FormLabel>
+                        <FormCheckInput type = 'email' required/>
+                        <FormLabel htmlFor='for'>Select your Doctor</FormLabel>
+                        <FormCheckInput type = 'text' required/>
+                        <FormButton type = 'submit'>Book</FormButton>
+                    </Form>
+                </FormContent>
+            </FormWrap>
+        </Container>
     )
 }
 
-export default index
+export default BookA
