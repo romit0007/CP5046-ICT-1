944f07eb9d752d54ef3d228f7e35b40da879a9fe
romitb1996 <76085614+romitb1996@users.noreply.github.com>
2021-05-07 00:16:43 +1000

    random

--- src/components/Covid/index.js
+++ src/components/Covid/index.js
@@ -7,18 +7,18 @@ const Covid = () => {
                 <CovidCard>
                     <CovidIcon src={Cimg} />
                     <CovidH2>  </CovidH2>
-                        <p>COVID-19 UPDATE
+                    <p>COVID-19 UPDATE
                             <b>Mask updates - Brisbane, including Northern suburb and Central Coast.</b>
 
                             Further to this morning's press conference from the QLD premier,
                             masks are compulsory as a Condition of Entry for all clinics in
                             Brisbane, including Northern suburb and Central Coast (effective
-                            immediately until 12:01 am Monday, May 20). 
+                            immediately until 12:01 am Monday, May 20).
 
                             This is in the interest of both our customers' and team members'
                             safety, which remains our priority. Customers may remove their mask
                             whilst having a face treatment e.g. facials, microdermabrasion,
-                            injectables. Face masks will also be mandatory for all staff. 
+                            injectables. Face masks will also be mandatory for all staff.
 
                             Our Medical Advisory Committee has also recommended that
                             temperature checks are a condition of entry for all customers in our
@@ -41,3 +41,5 @@ const Covid = () => {
                 </CovidCard>
             </CovidWrapper>
         </CovidContainer>
+    )
+}
