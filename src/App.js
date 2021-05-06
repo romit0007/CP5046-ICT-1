da8e327bf6572e67ba80e2fbe68b4678e9055fcb
romitb1996 <76085614+romitb1996@users.noreply.github.com>
2021-05-06 23:51:56 +1000

    Covid updates page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BApage from './pages/BA';
 import Mservices from './pages/Mservices';
 import ContactPage from './pages/Contact';
 import Policy from './pages/Policy';
+import Covid from './pages/Covid';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/MServices' component={Mservices} exact />
         <Route path='/Contact' component={ContactPage} exact />
         <Route path='/Policy' component={Policy} exact/> 
+        <Route path='/Covid' component={Covid} exact /> 
       </Switch>
     </Router>
   );
---
+++ src/pages/Covid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Covid from '../../components/Covid'
+
+const Covid = () => {
+    return (
+        <>
+            <Navbar />
+            <PageBanner pageTitle="COVID-19" />
+            <Covid />
+        </>
+    )
+}
+
+export default Covid
